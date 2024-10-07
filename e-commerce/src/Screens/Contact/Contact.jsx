5548b20833e4f6ce55458bd754db100eb945d3d0
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2024-10-07 14:44:33 +0500

    code

--- e-commerce/src/Screens/Contact/Contact.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export const Contact = () => {
-  return (
-<div style={{height:'90vh'}} className='d-flex justify-content-center align-items-center fs-1 fw-bold'>Contact</div>  )
-}
